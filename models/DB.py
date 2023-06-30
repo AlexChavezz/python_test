8396ca04bb6eddab7820cb6549349ceb6ef01cfc
AlexChavezz <alexischavez1234.ac@gmail.com>
2023-06-30 02:17:12 -0600

    models/DB.py getting-environment-variables modify

--- models/DB.py
+++ models/DB.py
@@ -5,7 +5,7 @@ class DB:
     def __init__(self) -> None:
         self.connection = None
         # do not forget to change the connection string after creating the cluster
-        self.url = f"mongodb+srv://{os.environ.get('user_name')}:{os.environ.get('password')}@myatlasclusteredu.fz1s1hm.mongodb.net/?retryWrites=true&w=majority"
+        self.url = f"mongodb+srv://{os.getenv('user_name')}:{os.getenv('password')}@myatlasclusteredu.fz1s1hm.mongodb.net/?retryWrites=true&w=majority"
     def get_all_questions(self):
         try:
             client = MongoClient(self.url)
